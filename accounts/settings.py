9df5abfc12019f5594f9868148efca8357b24941
Samuel Owoaanu <sam.adebayoadesina@gmail.com>
2025-10-20 21:06:34 +0100

    allowed all hosts

--- accounts/settings.py
+++ accounts/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'django-insecure-p3)2xc2g1rr*s%_xs8j)$kz4b3md_7x9nw*loag@ptkad)8l+_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
